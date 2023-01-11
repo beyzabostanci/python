d5a213128145862271457288033ac32a461bb81b
beyzabostanci <beyza_bostanc@hotmail.com>
2023-01-12 00:35:39 +0300

    tuples

--- pr4.py
+++ pr4.py
@@ -78,3 +78,58 @@ print("counts", counts)#counts is the dictionary
 jjj = { "chuck":1, "fred":42, "reha":89}
 for aaa,bbb in jjj.items():
     print(aaa, bbb)# aaa is for the names(key) and bbb is for the numbers(values)
+
+# TUPLES
+
+#z = (5,4,3)
+#z[2]=0
+
+#tuples and assignment
+(x,y)=("beyza", 23)
+print(x)
+
+print((1,2,3)<(4,5,6))
+
+#sorting lists of tuples
+
+d={"a":10, "b":1, "c":22}
+t = sorted(d.items())
+
+[("a", 10), ("b", 1), ("c", 22)]
+for k,v in sorted(d.items()):
+    print(k,v)
+
+#top 10 most common words in a file
+fhand = open("pr3.py")
+counts = dict()
+for line in fhand:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word,0)+1
+
+lst = list()
+for key,val in counts.items():
+    newtup = (val, key)
+    lst.append(newtup)
+
+lst = sorted(lst, reverse=True)
+
+for val, key in lst[:10]:
+    print(key,val)
+
+#shorter version for middle part
+c={"a":10, "b":1, "c":22}
+print(sorted([(v,k) for k,v in c.items()]))
+
+
+#1 and 2 are the same
+#1
+lst = []
+for key, val in counts.items():
+    newtup = (val, key)
+    lst.append(newtup)
+lst = sorted(lst, reverse=True)
+print(lst)
+
+#2
+print( sorted( [ (v,k) for k,v in counts.items() ], reverse=True ) )
